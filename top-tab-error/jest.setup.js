a885c8faa5dfbcd948e75beab14f8330e58bd46f
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2022-03-08 20:39:31 +0000

    Removed some extraneous code.

--- top-tab-error/jest.setup.js
+++ top-tab-error/jest.setup.js
@@ -13,20 +13,4 @@ jest.mock('react-native-reanimated', () => {
 });
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
-
-/* eslint-env jest */
-/* eslint-disable import/no-extraneous-dependencies */
-
-
-// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-
-const error = console.error;
-
-console.error = (...args) =>
-  // Suppress error messages regarding error boundary in tests
-  /(Consider adding an error boundary to your tree to customize error handling behavior|React will try to recreate this component tree from scratch using the error boundary you provided|Error boundaries should implement getDerivedStateFromError)/m.test(
-    args[0]
-  )
-    ? void 0
-    : error(...args);
