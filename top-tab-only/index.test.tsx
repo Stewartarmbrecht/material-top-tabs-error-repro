03bf558809ea386c53ef31391496f62c3e172d1f
Stewart Armbrecht (BBB) <stewart@boundbybetter.com>
2022-03-10 04:12:05 +0000

    Updated test text.

--- top-tab-only/index.test.tsx
+++ top-tab-only/index.test.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import { fireEvent, within, render } from '@testing-library/react-native';
 import App from './App';
 
-describe('Activity Entries Screen', () => {
-  
-  describe('With an existing activity', () => {
-    it('should display the list of entries', async () => {
-      let app = render(<App />);
-      const button = await app.findByTestId('SettingsTab')
-      await fireEvent.press(button);
-      const screen = await app.findByTestId("SettingsScreen");
-      await within(screen).findByText('Settings');
-    });
+describe('App', () => {
+  it('should display the settings screen when the user clicks the Settings tab', async () => {
+    let app = render(<App />);
+    const button = await app.findByTestId('SettingsTab')
+    await fireEvent.press(button);
+    const screen = await app.findByTestId("SettingsScreen");
+    await within(screen).findByText('Settings');
+  });
 
-  })
-    
 });
